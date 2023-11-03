e3b1376b0c0e262c6ab055ec1b05de134d44024a
Cheonji Kim <76100119+CheonjiKim@users.noreply.github.com>
2023-11-03 17:36:26 +0900

    곡 장르별 가중치 구현 완료
    [4, 5, 6, 4]
    케이팝, 발라드, 팝송, 힙합 순서

--- main.py
+++ main.py
@@ -1,76 +1,52 @@
 import time
-import winsound
-from src.game_pre import *
+from src.game_out import *
 from src.game_in import *
 from src.music_data import korpop_track, ballad_track, pop_track, hiphop_track
 
 # 장르의 순서는 다음과 같다: 
 all_tracks = [korpop_track, ballad_track, pop_track, hiphop_track]
+points_for_genres = [4, 5, 6, 4]
 
-print_genres()
-track = all_tracks[get_genre_idx()]
+
 
 
 #korpop이 선택되었다고 치자 일단은.
 track = korpop_track
 
-chance = 3
-score = 0
+
 
 #일단 lovelee라고 치자
 song = track[0]
-
+part = [song.get_part1(), song.get_part2(), song.get_part3()]
 # 배열에서 노래 하나를 선택하고, 그 노래를 배열에서 제거한다.
 # 러브리로 테스트하고나서는 실전에선 이걸 써야해 song = take_a_randomsong_from_track(track)
 
 
-part = [song.get_part1(), song.get_part2(), song.get_part3()]
-
-i = 0
-
+score = 0 # 이게 10점에 도달해야 한다.
 time.sleep(2)
 print("=========================")
-print("게임을 시작합니다.")
+print("지금부터 게임을 시작합니다.")
 print("=========================")
+time.sleep(2)
 print()
-time.sleep(2)
 
+# 이 while문에서 게임이 진행된다.
+while(score < 10):
+    print_genres()
+    genre_idx = get_genre_idx()
+    track = all_tracks[genre_idx]
+    point = points_for_genres[genre_idx]
+    
+    score += play_round(track, point)
+    
 
-while(chance > 0):
-    print("(♪ 곡의 일부 재생중 ♪)")
-    winsound.PlaySound(part[i], 0) # 재생
-    guess_title = input_guess()
-    song_title = song.get_title()
-    correct = is_guess_correct(song_title, guess_title)
-    time.sleep(1)
-    if (correct): 
-        score += 1
-        print("정답을 맞혔습니다!")
-        print("해당 곡은", song.get_author(), "의", song.get_title(), "입니다.")
-        break
-    else: # 정답을 맞히지 못한 경우
-        chance -= 1
-        i += 1
-        
-        print("정답이 아닙니다. 기회가 하나 차감됩니다.")
-        print("남은 기회 수:", chance)
-        print()
-        if(chance > 0):
-            time.sleep(1)
-            print("(해당 곡의 다른 부분을 재생합니다.)")
-            print()
-        else:
-            print("기회를 모두 소진하였습니다.")
-            print("해당 곡은", song.get_author(), "의", song.get_title(), "입니다.")
-            print()
-    time.sleep(2)
-
-if(score >= 1):
-    print("축하합니다. 승리하셨습니다!")
-    exit()
-
-if(chance <= 0):
-    print("패배하셨습니다.")
-    exit()
+# 승패 판정
+if(score >= 10):
+# 결국 스코어가 10이 넘어야 이긴다.
+    end_win_game()
+else:
+# 이 구문에 걸리는 경우는 사실 없을 것이다. while문에서 10점 도달할 때까지 빠져나오지 못하기 때문이다.
+#   while문에서 10점 도달할 때까지 빠져나오지 못하기 때문이다. 
+    end_lose_game()
 
 
--- src/game_in.py
+++ src/game_in.py
@@ -1,4 +1,6 @@
 import random
+import winsound
+import time
 
 def input_guess():
     guess = input("이 곡의 제목을 입력하세요 : ")
@@ -19,3 +21,62 @@ def take_a_randomsong_from_track(tr):
     song = tr[rand_idx]
     tr.remove(song)
     return song
+
+def end_lose_game():
+    print("========================")
+    print("패배하셨습니다. 게임을 종료합니다.")
+    print("========================")
+    exit()
+    
+def end_win_game():
+    print("========================")
+    print("목표 점수에 도달하셨습니다.")
+    print("승리하셨습니다. 게임을 종료합니다.")
+    print("========================")
+    exit()
+
+def play_round(track, point):
+# 해당 장르의 트랙과, 그 장르의 기본 포인트를 매개변수로 넣는다.
+# 세 번 음악을 틀고 그때마다 제목을 물어본다. 음악을 못 맞출 때마다 1점씩 깎는다.
+# 음악을 맞히면 그에 맞는 점수를 반환한다.
+# 세 번의 기회가 끝나면 패배 선언한다.
+    if (track == []):
+        print("해당 장르 트랙의 모든 곡을 사용했습니다.")
+        
+    song = take_a_randomsong_from_track(track)
+    part = [song.get_part1(), song.get_part2(), song.get_part3()]
+    chance = 3
+    i = 0
+    
+    while(chance > 0):
+        print("(♪ 곡의 일부 재생중 ♪)")
+        winsound.PlaySound(part[i], 0) # 재생
+        guess_title = input_guess()
+        song_title = song.get_title()
+        correct = is_guess_correct(song_title, guess_title)
+        time.sleep(1)
+        
+        if (correct): 
+            score += 1
+            print("정답을 맞혔습니다!")
+            print("해당 곡은", song.get_author(), "의", song.get_title(), "입니다.")
+            return point
+        else: # 정답을 맞히지 못한 경우
+            point -= 1
+            chance -= 1
+            i += 1
+            
+            print("정답이 아닙니다. 기회가 하나 차감됩니다.")
+            print("남은 기회 수:", chance)
+            print()
+            if(chance > 0):
+                time.sleep(1)
+                print("(해당 곡의 다른 부분을 재생합니다.)")
+                print()
+            else:
+                print("기회를 모두 소진하였습니다.")
+                print("해당 곡은", song.get_author(), "의", song.get_title(), "입니다.")
+                print()
+                lose_game(score)
+        time.sleep(2)
+    return point
